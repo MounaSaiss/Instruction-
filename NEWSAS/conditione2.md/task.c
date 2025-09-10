6d6a6a11031f17328a2d34bd0bdef619cc1d4884
LunaDevX <mouna1.saiss@gmail.com>
2025-09-10 23:48:42 +0100

    first

---
+++ NEWSAS/conditione2.md/task.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main ()
+{
+    int R,SC,DP;
+    printf("Veuillez saisir revenu annuel:");
+    scanf("%d",&R);
+    printf("Veuillez saisir le score de credit :");
+    scanf("%d",&SC);
+    printf("Veuillez saisr Durée de pret:");
+    scanf("%d",&DP);
+    if (R>= 30000 && SC >= 700 &&  DP <= 10 )
+       printf ("Eligible");
+    else if (R>= 30000 && SC>= 650 && DP<= 15)
+       printf("Eligible avec conditions");
+    else if (R<30000  || SC<650 || DP > 15  )
+       printf("Non Eligible");
+    else 
+       printf("pas de reponse ressayer autre fois");
+    return 0; 
+}
