312b24d8a90af9f9cfb84f18b9777b1fc1473dc3
LunaDevX <mouna1.saiss@gmail.com>
2025-09-11 21:10:21 +0100

    go

---
+++ Boucles.md/challenge4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+    int k,x,i;
+    printf("Veuillez saisir un nombre :");
+    scanf("%d",&x);
+    for (i=1;i<=x;i++){
+        k=2*i-1;
+   
+   printf(" %d",k);
+    }
+   
+    
+    return 0;
+}
